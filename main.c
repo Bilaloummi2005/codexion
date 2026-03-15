f4d0e3e29866dbac79cf32b0060bab82a3e58a0f
Bilal Oummi <boummi@e2r10p11.1337.ma>
2026-03-15 07:46:05 +0000

    adding monitor thread that check all the threads for burnout and compile turns

--- main.c
+++ main.c
@@ -32,16 +32,18 @@ typedef struct s_args
     long time_to_refactor;
     long number_of_compiles_required;
     long dongle_cooldown;
+    long n_coders;
     char* scheduler;
+    int burned_out;
 } t_args;
 
 typedef struct s_thread
 {
     int id;
-    int turns;
+    int compile_count;
     long start_time;
     long last_compile;
-    t_args requirements;
+    t_args *requirements;
     t_dongle *right_dongle;
     t_dongle *left_dongle;
 } t_thread;
@@ -58,31 +60,14 @@ void* routine(void *arg)
 {
     t_thread *data = (t_thread *)arg;
     int i = 0;
-    int turns = data->turns;
-    int time_to_burnout = data->requirements.time_to_burnout;
-    int time_to_compile = data->requirements.time_to_compile;
-    int time_to_debug = data->requirements.time_to_debug;
-    int time_to_refactor = data->requirements.time_to_refactor;
-    int dongle_cooldown = data->requirements.dongle_cooldown;
-    while(turns--){
+    int *turns = &data->compile_count;
+    int time_to_burnout = data->requirements->time_to_burnout;
+    int time_to_compile = data->requirements->time_to_compile;
+    int time_to_debug = data->requirements->time_to_debug;
+    int time_to_refactor = data->requirements->time_to_refactor;
+    int dongle_cooldown = data->requirements->dongle_cooldown;
 
-        // printf("befor sleep right coldown %ld %ld coder %d\n", get_time() - data->right_dongle->last_release, get_time() - data->left_dongle->last_release < data->requirements.dongle_cooldown, data->id);
-        // printf("befor sleep left coldown %ld %ld coder %d\n", get_time() - data->left_dongle->last_release, get_time() - data->left_dongle->last_release < data->requirements.dongle_cooldown, data->id);
-        
-        // if (get_time() - data->right_dongle->last_release < data->requirements.dongle_cooldown)
-        // {
-        //     printf("right coldown %ld %d coder %d\n", get_time() - data->right_dongle->last_release, get_time() - data->left_dongle->last_release < data->requirements.dongle_cooldown, data->id);
-        //     usleep((data->requirements.dongle_cooldown - (get_time() - data->right_dongle->last_release)) * 1000);
-        //     printf("right coldown %ld %d coder %d\n", get_time() - data->right_dongle->last_release, get_time() - data->left_dongle->last_release < data->requirements.dongle_cooldown, data->id);
-
-        // }
-        // if (get_time() - data->left_dongle->last_release < data->requirements.dongle_cooldown)
-        // {
-        //     printf("after sleep left coldown %d %d coder %d\n", get_time() - data->left_dongle->last_release, get_time() - data->left_dongle->last_release < data->requirements.dongle_cooldown, data->id);
-        //     usleep((data->requirements.dongle_cooldown - (get_time() - data->left_dongle->last_release)) * 1000);
-        //     printf("after sleep left coldown %d %d coder %d\n", get_time() - data->left_dongle->last_release, get_time() - data->left_dongle->last_release < data->requirements.dongle_cooldown, data->id);
-
-        // }
+    while(!data->requirements->burned_out){
         if (data->id % 2){
             // printf("%d %d\n", get_time() - data->right_dongle->last_release, data->requirements.dongle_cooldown);
             pthread_mutex_lock(&data->right_dongle->mutex);
@@ -92,11 +77,6 @@ void* routine(void *arg)
             pthread_mutex_lock(&data->left_dongle->mutex);
             pthread_mutex_lock(&data->right_dongle->mutex);
         }
-        // printf("right coldown %d %d coder %d\n", get_time() - data->right_dongle->last_release, get_time() - data->left_dongle->last_release < data->requirements.dongle_cooldown, data->id);
-        // printf("left coldown %d %d coder %d\n", get_time() - data->left_dongle->last_release, get_time() - data->left_dongle->last_release < data->requirements.dongle_cooldown, data->id);
-        
-        // printf("%d has taken right dongle.mutex\n", data->id);
-        // printf("%d has taken left dongle.mutex\n", data->id);
         printf("diff bet last compile and now %ld, time to burnout %d\n", get_time() - data->last_compile, time_to_burnout);
         if (get_time() - data->last_compile > time_to_burnout)
         {
@@ -120,10 +100,37 @@ void* routine(void *arg)
 
         printf(BLU "%d is refactoring\n" RESET, data->id);
         usleep(time_to_refactor * 1000);
-        printf(YEL "coder %d turn %d\n" RESET, data->id, i);
+        printf(YEL "coder %d turn %d\n" RESET, data->id, *turns);
+        data->compile_count++;
         // usleep(500);
     }
     return NULL;
+}
+
+void* monitor(void *arg){
+    t_thread *coders = (t_thread *)arg;   // correct cast
+    int n = coders[0].requirements->n_coders;
+    long time_to_burnout = coders[0].requirements->time_to_burnout;
+    long required = coders[0].requirements->number_of_compiles_required; 
+    int done;
+    while (1){
+        usleep(1000);
+        for (int i=0; i < n; i++){
+            if (get_time() - coders[i].last_compile > time_to_burnout){
+                coders[i].requirements->burned_out = 1;
+                return NULL;
+            }
+        }
+        done = 1;
+        for (int i=0; i < n; i++){
+            if (coders[i].compile_count < required)
+                done = 0;
+        }
+        if (done){
+            coders[0].requirements->burned_out = 1;
+            return NULL;
+        }
+    }
 }
 
 
@@ -136,6 +143,7 @@ int main(int argc, char* argv[]) {
 
     int n = atoi(argv[1]);
     t_args requirements;
+    requirements.n_coders = n;
     requirements.time_to_burnout = atoi(argv[2]);
     requirements.time_to_compile = atoi(argv[3]);
     requirements.time_to_debug = atoi(argv[4]);
@@ -143,28 +151,34 @@ int main(int argc, char* argv[]) {
     requirements.number_of_compiles_required = atoi(argv[6]);
     requirements.dongle_cooldown = atoi(argv[7]);
     requirements.scheduler = argv[8];
+    requirements.burned_out = 0;
     pthread_t t[n];
     t_thread data[n];
     t_dongle mutex[n];
+    pthread_t m;
 
     for (int i = 0; i < n; i++){
         mutex[i].last_release = get_time() - requirements.dongle_cooldown;
         pthread_mutex_init(&mutex[i].mutex, NULL);
     }
     
+
     for (int i = 0; i < n; i++){
         // printf("thread %d started execution:\n", i);
         data[i].id = i + 1;
-        data[i].turns = requirements.number_of_compiles_required;
+        data[i].compile_count = 0;
         data[i].left_dongle = &mutex[i];
         data[i].right_dongle = &mutex[(i + 1) % n];
         data[i].start_time = get_time();
-        data[i].requirements = requirements;
+        data[i].requirements = &requirements;
         data[i].last_compile = get_time();
-        if (pthread_create(&t[i], NULL, &routine, &data[i])) {
+        if (pthread_create(&t[i], NULL, &routine, &data[i])) 
             return 1;
-        }
     }
+    if (pthread_create(&m, NULL, &monitor, &data))
+        return 1;
+    if (pthread_join(m, NULL)) 
+        return 1;
 
     for (int i = 0; i < n; i++){
         if (pthread_join(t[i], NULL)) {
